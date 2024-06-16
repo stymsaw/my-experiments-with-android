d1e353eafe8e5395d49b30d08f3e5414d69f52db
Satyam Saw <85004499+stymsaw@users.noreply.github.com>
2024-06-16 19:59:14 +0530

    uploaded android projects
    - android launch modes
    - android runtime permissions

---
+++ AndroidLaunchModes/app/src/main/java/com/tutorial/launch_modes/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.tutorial.launch_modes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.tutorial.launch_modes.ui.theme.AndroidLaunchModesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AndroidLaunchModesTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AndroidLaunchModesTheme {
+        Greeting("Android")
+    }
+}

fa65a41022a5019318160e6ebb8b5939031ee24f
Satyam Saw <satyamkumarsaw30@gmail.com>
2024-06-16 20:37:31 +0530

    updated launch modes project

--- AndroidLaunchModes/app/src/main/java/com/tutorial/launch_modes/MainActivity.kt
+++ AndroidLaunchModes/app/src/main/java/com/tutorial/launch_modes/MainActivity.kt
@@ -22,7 +22,8 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android Launch Modes",
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier
+                            .padding(innerPadding)
                     )
                 }
             }
